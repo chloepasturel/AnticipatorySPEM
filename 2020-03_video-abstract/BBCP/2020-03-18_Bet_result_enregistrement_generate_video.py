07be7808ae7c1e624556293db8e280f0502b7605
Laurent Perrinet <laurent.perrinet@gmail.com>
2020-03-25 17:24:41 +0100

    Building video 2_results_enregistrement.mp4.

--- 2020-03_video-abstract/BBCP/2020-03-18_Bet_result_enregistrement_generate_video.py
+++ 2020-03_video-abstract/BBCP/2020-03-18_Bet_result_enregistrement_generate_video.py
@@ -18,8 +18,8 @@ def video():
     # ---------------------------------------------------
     # setup values
     # ---------------------------------------------------
-    screen_width_px = 800/1.618
-    screen_height_px = 500/1.618
+    screen_width_px = 1314 # 885 #800#/1.618
+    screen_height_px = 881 # 589 # 500#/1.618
     framerate = 60
 
     # ---------------------------------------------------
@@ -47,7 +47,7 @@ def video():
     clock = core.Clock()
 
     for num_fig in range(1, len(fileList)+1) :
-        fig = visual.ImageStim(win, folder + '2_results_enregistrement_%s.png'%num_fig)
+        fig = visual.ImageStim(win, os.path.join(folder, '2_results_enregistrement_%s.png' % num_fig))
         fig.setPos((offset, offset))
 
         if num_fig<num_fig1_bascule :   wait = wait0
@@ -76,7 +76,7 @@ def video():
     for n, frame in enumerate(win.movieFrames):
         win.movieFrames[n] = np.array(frame)
     clip = ImageSequenceClip(win.movieFrames, fps=framerate)
-    clip.write_videofile('%s.mp4'%(NameVideo))
+    clip.write_videofile('%s.mp4' % (NameVideo))
 
     win.close()
     core.quit()
